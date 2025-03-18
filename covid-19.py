af615ab3140f42726f8c2ada6f1f1be729b6d76d
Ajara-cloud <aabdul-rahaman@aims.edu.gh>
2025-03-18 15:23:10 +0000

    First Commit -  Added covid-19.py

---
+++ covid-19.py
@@ -0,0 +1,17 @@
+import requests
+from win10toast import ToastNotifier
+import json
+import time
+
+
+def update():
+    r = requests.get(' https://disease.sh/v3/covid-19/all')
+    data = r.json()
+    text = f'Confirmed Cases: {data['cases']}\nDeaths: {data['deaths']}\nRecovered: {data['recovered']}'
+
+    while True:
+        toast = ToastNotifier()
+        toast.show_toast('Covid-19 Updates', text, duration=10)
+        time.sleep(60 * 60)
+        
+update()
